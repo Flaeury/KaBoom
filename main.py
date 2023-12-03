fb448ecacda7c58c09f8c6f60856fbb720980985
Raiane <raianw200410@outlook.com>
2023-12-03 00:03:36 -0300

    inicio do projeto

--- main.py
+++ main.py
@@ -1,11 +1,72 @@
 import flet as ft
-from pages.index import index
 
-def main(page: ft.Page):        
-    page.title = "TechTemple"             
-    page.theme_mode = "dark"
-    
-    # Chame a função ou crie uma instância da classe do arquivo index.py
-    index(page)
+
+def main(page: ft.Page):
+    def check_item_clicked(e):
+        e.control.checked = not e.control.checked
+        page.update()
+
+    # NavigationRail
+    rail = ft.NavigationRail(
+        selected_index=0,
+        label_type=ft.NavigationRailLabelType.ALL,
+        min_width=100,
+        min_extended_width=400,
+        leading=ft.FloatingActionButton(icon=ft.icons.CREATE, text="Add"),
+        group_alignment=-0.9,
+        destinations=[
+            ft.NavigationRailDestination(
+                icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="First"
+            ),
+            ft.NavigationRailDestination(
+                icon_content=ft.Icon(ft.icons.BOOKMARK_BORDER),
+                selected_icon_content=ft.Icon(ft.icons.BOOKMARK),
+                label="Second",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.icons.SETTINGS_OUTLINED,
+                selected_icon_content=ft.Icon(ft.icons.SETTINGS),
+                label_content=ft.Text("Settings"),
+            ),
+        ],
+        on_change=lambda e: print(
+            "Selected destination:", e.control.selected_index),
+    )
+
+    # AppBar
+    page.appbar = ft.AppBar(
+        # leading=ft.Icon(ft.icons.PALETTE),
+        leading_width=40,
+        title=ft.Text("TechTemple"),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+            # ft.IconButton(ft.icons.FILTER_3),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(text="Item 1"),
+                    ft.PopupMenuItem(),  # divider
+                    ft.PopupMenuItem(
+                        text="Checked item", checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            ),
+        ],
+    )
+
+    # Layout principal
+    page.add(
+        ft.Row(
+            [
+                rail,
+                ft.VerticalDivider(width=1),
+                ft.Column([ft.Text("Body!")],
+                          alignment=ft.MainAxisAlignment.START, expand=True),
+            ],
+            expand=True,
+        )
+    )
+
 
 ft.app(target=main)
