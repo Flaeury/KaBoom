d33d418c7a5f16b2fa307c9c69a65fe3858dbdb4
raiane <hi.alita1999@gmail.com>
2023-12-04 10:38:17 -0300

    cards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import flet as ft
 
-
 def main(page: ft.Page):
     def check_item_clicked(e):
         e.control.checked = not e.control.checked
@@ -10,7 +9,7 @@ def main(page: ft.Page):
     rail = ft.NavigationRail(
         selected_index=0,
         label_type=ft.NavigationRailLabelType.ALL,
-        min_width=100,
+        min_width=120,
         min_extended_width=400,
         leading=ft.FloatingActionButton(icon=ft.icons.CREATE, text="Add"),
         group_alignment=-0.9,
@@ -29,20 +28,18 @@ def main(page: ft.Page):
                 label_content=ft.Text("Settings"),
             ),
         ],
-        on_change=lambda e: print(
-            "Selected destination:", e.control.selected_index),
+        on_change=lambda e: print("Selected destination:", e.control.selected_index),
     )
 
     # AppBar
     page.appbar = ft.AppBar(
-        # leading=ft.Icon(ft.icons.PALETTE),
+        leading=ft.Icon(name=ft.icons.GAMES, color=ft.colors.BLUE),
         leading_width=40,
         title=ft.Text("TechTemple"),
         center_title=False,
         bgcolor=ft.colors.SURFACE_VARIANT,
         actions=[
             ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
-            # ft.IconButton(ft.icons.FILTER_3),
             ft.PopupMenuButton(
                 items=[
                     ft.PopupMenuItem(text="Item 1"),
@@ -55,18 +52,130 @@ def main(page: ft.Page):
         ],
     )
 
+    # Responsive Cards
+    cards = ft.Row(
+        [
+            ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable without Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor=ft.colors.GREEN_200,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        on_click=lambda e: print("Clickable without Ink clicked!"),
+                    ),
+                ]
+            ),
+            ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable with Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor=ft.colors.CYAN_200,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        ink=True,
+                        on_click=lambda e: print("Clickable with Ink clicked!"),
+                    ),
+                ]
+            ),
+            ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable transparent with Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        ink=True,
+                        on_click=lambda e: print("Clickable transparent with Ink clicked!"),
+                    ),
+                ]
+            ),
+             ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable with Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor=ft.colors.CYAN_200,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        ink=True,
+                        on_click=lambda e: print("Clickable with Ink clicked!"),
+                    ),
+                ]
+            ),
+            ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable transparent with Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        ink=True,
+                        on_click=lambda e: print("Clickable transparent with Ink clicked!"),
+                    ),
+                ]
+            ),
+             ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable without Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor=ft.colors.GREEN_200,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        on_click=lambda e: print("Clickable without Ink clicked!"),
+                    ),
+                ]
+            ),
+            ft.Column(
+                [
+                    ft.Container(
+                        content=ft.Text("Clickable with Ink"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor=ft.colors.CYAN_200,
+                        width=190,
+                        height=245,
+                        border_radius=10,
+                        ink=True,
+                        on_click=lambda e: print("Clickable with Ink clicked!"),
+                    ),
+                ]
+            ),
+             
+          
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        spacing=10, 
+    )
+
     # Layout principal
     page.add(
         ft.Row(
-            [
-                rail,
-                ft.VerticalDivider(width=1),
-                ft.Column([ft.Text("Body!")],
-                          alignment=ft.MainAxisAlignment.START, expand=True),
-            ],
+            [rail, ft.VerticalDivider(width=0.1), cards],
             expand=True,
         )
     )
-
 
 ft.app(target=main)
