846d594eadf4eccc99dd9da7b53d8e09fb857999
carolinalimaal <carol.lima.alves2005@gmail.com>
2023-12-08 12:57:15 -0300

    atualização app bar

--- assets/components/appBar.py
+++ assets/components/appBar.py
@@ -2,25 +2,27 @@ import flet as ft
 
 
 def create_app_bar():
-
     return ft.AppBar(
+        title=ft.Text(
+                        value='TechTemple',
+                        color='#FFFFFF',
+                        font_family='assets/fonts/BungeeSpice',
+                        size=45,
+                        italic=True,
+        ),
+        leading=ft.Icon(
+                        name=ft.icons.GAMEPAD_ROUNDED,
+                        size=25,
+                        color='#FF7E20',
+                        scale=10
+            ),
         bgcolor='#0C4B85',
-
-        toolbar_height=70,
-
-        title=ft.Text('TechTemple', color=ft.colors.ORANGE_600,
-                      font_family='LuckiestGuy'),
-        # logo da plataforma
-        leading=ft.Icon(name=ft.icons.VIDEOGAME_ASSET_OUTLINED,
-                        color=ft.colors.ORANGE_600, size=50),
-        leading_width=100,
         actions=[
-            # icone do carrinho - adicionar a mudança de rota ainda
-            ft.IconButton(ft.icons.SHOPPING_CART_ROUNDED,
-                          tooltip='Ir para o carrinho',
-                          icon_color='#FFFFFF',
-                          icon_size=30,
-                          ),
-
-        ],
+                ft.IconButton(
+                    ft.icons.SHOPPING_CART_ROUNDED,
+                    tooltip='Ir para o carrinho',
+                    icon_color='#FFFFFF',
+                    icon_size=30,
+                )
+        ]
     )
--- main.py
+++ main.py
@@ -11,28 +11,31 @@ def main(page: ft.Page):
             ft.View(
                 "/",
                 [
-                    ft.AppBar(
-                        bgcolor='#0C4B85',
-
-                        toolbar_height=70,
-
-                        title=ft.Text('TechTemple', color=ft.colors.ORANGE_600,
-                                      font_family='LuckiestGuy'),
-                        # logo da plataforma
-                        leading=ft.Icon(name=ft.icons.VIDEOGAME_ASSET_OUTLINED,
-                                        color=ft.colors.ORANGE_600, size=50),
-                        leading_width=100,
-                        actions=[
-                            # icone do carrinho - adicionar a mudança de rota ainda
-                            ft.IconButton(ft.icons.SHOPPING_CART_ROUNDED,
-                                          tooltip='Ir para o carrinho',
-                                          icon_color='#FFFFFF',
-                                          icon_size=30,
-                                          on_click=lambda _: page.go("/cart"),
-                                          ),
-
-                        ],
-                    ),
+                ft.AppBar(
+                        title=ft.Text(
+                                    value='TechTemple',
+                                    color='#FFFFFF',
+                                    font_family='assets/fonts/BungeeSpice',
+                                    size=45,
+                                    italic=True,
+                                ),
+                        leading=ft.Icon(
+                                    name=ft.icons.GAMEPAD_ROUNDED,
+                                    size=25,
+                                    color='#FF7E20',
+                                    scale=10
+                                ),
+                            bgcolor='#0C4B85',
+                            actions=[
+                                    ft.IconButton(
+                                            ft.icons.SHOPPING_CART_ROUNDED,
+                                            tooltip='Ir para o carrinho',
+                                            icon_color='#FFFFFF',
+                                            icon_size=30,
+                                            on_click=lambda _: page.go("/cart")
+                                        )
+                                    ]
+                                ),
                     dashboard,
                 ],
             ),
@@ -44,23 +47,30 @@ def main(page: ft.Page):
                     "/cart",
                     [
                         ft.AppBar(
+                            title=ft.Text(
+                                        value='TechTemple',
+                                        color='#FFFFFF',
+                                        font_family='assets/fonts/BungeeSpice',
+                                        size=45,
+                                        italic=True,
+                                    ),
+                            leading=ft.Icon(
+                                        name=ft.icons.GAMEPAD_ROUNDED,
+                                        size=25,
+                                        color='#FF7E20',
+                                        scale=10
+                                        ),
                             bgcolor='#0C4B85',
-
-                            toolbar_height=70,
-
-                            title=ft.Text('TechTemple', color=ft.colors.ORANGE_600,
-                                          font_family='LuckiestGuy'),
-                            # logo da plataforma
-                            leading=ft.Icon(name=ft.icons.VIDEOGAME_ASSET_OUTLINED,
-                                            color=ft.colors.ORANGE_600, size=50),
-                            leading_width=100,
                             actions=[
-                                # icone do carrinho - adicionar a mudança de rota ainda
-                                ft.ElevatedButton(
-                                    "Voltar", on_click=lambda _: page.go("/")),
-
-                            ],
-                        ),
+                                    ft.IconButton(
+                                            ft.icons.SHOPPING_CART_ROUNDED,
+                                            tooltip='Ir para o carrinho',
+                                            icon_color='#FFFFFF',
+                                            icon_size=30,
+                                            on_click=lambda _: page.go("/cart")
+                                        )
+                                    ]
+                                ),
                         # ALGO AQUI PARA O CART
                     ],
                 )
